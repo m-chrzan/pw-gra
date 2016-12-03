1d00fe5e5f8df77d37b10e62a497aec3cdfa477b
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-12-03 14:14:26 +0100

    Implement Pozycja constructor and getters

--- gra/Pozycja.java
+++ gra/Pozycja.java
@@ -1,4 +1,19 @@
 package gra;
 
 public class Pozycja {
+    private int wiersz;
+    private int kolumna;
+
+    public Pozycja(int wiersz, int kolumna) {
+        this.wiersz = wiersz;
+        this.kolumna = kolumna;
+    }
+
+    public int wiersz() {
+        return wiersz;
+    }
+
+    public int kolumna() {
+        return kolumna;
+    }
 }
--- testy/PozycjaTests.java
+++ testy/PozycjaTests.java
@@ -3,4 +3,25 @@ package testy;
 import gra.Pozycja;
 
 public class PozycjaTests {
+    private static void beginTest(String name) {
+        Testing.beginTest("PozycjaTest." + name);
+    }
+
+    public static void testConstructor() {
+        beginTest("Constructor");
+
+        Pozycja p1 = new Pozycja(4, 2);
+        Testing.checkEqual(p1.wiersz(), 4, "Row set correctly");
+        Testing.checkEqual(p1.kolumna(), 2, "Column set correctly");
+
+        Pozycja p2 = new Pozycja(12, 24);
+        Testing.checkEqual(p2.wiersz(), 12, "Row set correctly");
+        Testing.checkEqual(p2.kolumna(), 24, "Column set correctly");
+
+        Testing.finishTest();
+    }
+
+    public static void main(String[] args) {
+        testConstructor();
+    }
 }
