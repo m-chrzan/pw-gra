86d86fabfb4274c28845b5e81daee92236b7a6f4
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-12-03 23:36:58 +0100

    Implement zawiera()

--- gra/PostaćNaPlanszy.java
+++ gra/PostaćNaPlanszy.java
@@ -1,13 +1,49 @@
 package gra;
 
 public class PostaćNaPlanszy {
-    Postać postać;
+    private Postać postać;
+    private Pozycja pozycja;
+    private Pozycja chcianaPozycja;
 
     public PostaćNaPlanszy(Postać postać) {
         this.postać = postać;
+        this.pozycja = Pozycja.dajPozycjęZaPlanszą();
+        this.chcianaPozycja = Pozycja.dajPozycjęZaPlanszą();
+    }
+
+    public void chcianaPozycja(Pozycja pozycja) {
+        chcianaPozycja = pozycja;
+    }
+
+    public void przesuń() {
+        pozycja = chcianaPozycja;
+        chcianaPozycja = Pozycja.dajPozycjęZaPlanszą();
     }
 
     public Postać dajPostać() {
         return postać;
+    }
+
+    public boolean zawiera(Pozycja pozycja) {
+        return pozycja.wiersz() >= górnaGranica() &&
+               pozycja.wiersz() <= dolnaGranica() &&
+               pozycja.kolumna() >= lewaGranica() &&
+               pozycja.kolumna() <= prawaGranica();
+    }
+
+    private int górnaGranica() {
+        return pozycja.wiersz();
+    }
+
+    private int lewaGranica() {
+        return pozycja.kolumna();
+    }
+
+    private int dolnaGranica() {
+        return pozycja.wiersz() + postać.dajWysokość() - 1;
+    }
+
+    private int prawaGranica() {
+        return pozycja.kolumna() + postać.dajSzerokość() - 1;
     }
 }
--- testy/PostaćNaPlanszyTests.java
+++ testy/PostaćNaPlanszyTests.java
@@ -1,11 +1,40 @@
 package testy;
 
 import gra.PostaćNaPlanszy;
+import gra.Pozycja;
 import sample.ProstaPostać;
 
 public class PostaćNaPlanszyTests {
     public static void beginTest(String name) {
         Testing.beginTest("PostaćNaPlanszy." + name);
+    }
+
+    public static void checkContainsAll(PostaćNaPlanszy p, int row,
+            int column, int height, int width, String msg) {
+        boolean containsAll = true;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (!p.zawiera(new Pozycja(row + i, column + j))) {
+                    containsAll = false;
+                }
+            }
+        }
+
+        Testing.checkTrue(containsAll, msg);
+    }
+
+    public static void checkContainsNone(PostaćNaPlanszy p, int row,
+            int column, int height, int width, String msg) {
+        boolean containsNone = true;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (p.zawiera(new Pozycja(row + i, column + j))) {
+                    containsNone = false;
+                }
+            }
+        }
+
+        Testing.checkTrue(containsNone, msg);
     }
 
     public static void testDajPostać() {
@@ -21,7 +50,28 @@ public class PostaćNaPlanszyTests {
                "PostaćNaPlanszy object refers to a specific Postać object");
     }
 
+    public static void testZawiera() {
+        beginTest("zawiera");
+
+        PostaćNaPlanszy postać = new PostaćNaPlanszy(new ProstaPostać(4, 2));
+        postać.chcianaPozycja(new Pozycja(1, 1));
+        postać.przesuń();
+
+        checkContainsAll(postać, 1, 1, 4, 2,
+                "Postać contains all squares inside it");
+        checkContainsNone(postać, 0, 0, 1, 4,
+                "Postać doesn't contain the squares above it");
+        checkContainsNone(postać, 1, 0, 4, 1,
+                "Postać doesn't contain the squares to its left");
+        checkContainsNone(postać, 5, 0, 1, 4,
+                "Postać doesn't contain the squares below it");
+        checkContainsNone(postać, 1, 3, 4, 1,
+                "Postać doesn't contain the squares to its right");
+    }
+
     public static void main(String[] args) {
         testDajPostać();
+        testZawiera();
     }
+
 }
