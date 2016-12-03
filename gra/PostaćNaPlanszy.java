b96370047aa87b791ddb72d46255a8c32756caa2
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-12-03 15:24:18 +0100

    Implement dajPostać()

--- gra/PostaćNaPlanszy.java
+++ gra/PostaćNaPlanszy.java
@@ -1,4 +1,13 @@
 package gra;
 
 public class PostaćNaPlanszy {
+    Postać postać;
+
+    public PostaćNaPlanszy(Postać postać) {
+        this.postać = postać;
+    }
+
+    public Postać dajPostać() {
+        return postać;
+    }
 }
--- testy/PostaćNaPlanszyTests.java
+++ testy/PostaćNaPlanszyTests.java
@@ -1,6 +1,27 @@
 package testy;
 
 import gra.PostaćNaPlanszy;
+import sample.ProstaPostać;
 
 public class PostaćNaPlanszyTests {
+    public static void beginTest(String name) {
+        Testing.beginTest("PostaćNaPlanszy." + name);
+    }
+
+    public static void testDajPostać() {
+        beginTest("dajPostać");
+
+        ProstaPostać pp1 = new ProstaPostać(4, 2);
+        ProstaPostać pp2 = new ProstaPostać(4, 2);
+
+        PostaćNaPlanszy postać = new PostaćNaPlanszy(pp1);
+
+        Testing.checkEqual(postać.dajPostać(), pp1, "Correct Postać reference kept");
+        Testing.checkNotEqual(postać.dajPostać(), pp2,
+               "PostaćNaPlanszy object refers to a specific Postać object");
+    }
+
+    public static void main(String[] args) {
+        testDajPostać();
+    }
 }
