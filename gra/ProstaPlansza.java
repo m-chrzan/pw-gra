2b8a0f08327d74be8c0752fbd5c526b2350cc31e
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-12-05 00:11:55 +0100

    Implement base ProstaPlansza methods
    - Constructor
    - chęćPostawienia
    - przesuńNaChcianąPozycję
    - sprawdź

--- gra/ProstaPlansza.java
+++ gra/ProstaPlansza.java
@@ -1,4 +1,64 @@
 package gra;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class ProstaPlansza {
+    private List<PostaćNaPlanszy> postacie;
+    private int wysokość;
+    private int szerokość;
+
+    public ProstaPlansza(int wysokość, int szerokość) {
+        postacie = new ArrayList<PostaćNaPlanszy>();
+        this.wysokość = wysokość;
+        this.szerokość = szerokość;
+    }
+
+    public void chęćPostawienia(Postać postać, int wiersz, int kolumna) {
+        PostaćNaPlanszy postaćNaPlanszy = new PostaćNaPlanszy(postać);
+        postaćNaPlanszy.chcianaPozycja(new Pozycja(wiersz, kolumna));
+        postacie.add(postaćNaPlanszy);
+    }
+
+    public void przesuńNaChcianąPozycję(Postać postać) {
+        try {
+            PostaćNaPlanszy postaćNaPlanszy = znajdźPostać(postać);
+            postaćNaPlanszy.przesuń();
+        } catch (NieZnalezionoPostaci np) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private PostaćNaPlanszy znajdźPostać(Postać postać) throws NieZnalezionoPostaci {
+        for (PostaćNaPlanszy postaćNaPlanszy : postacie) {
+            if (postaćNaPlanszy.dajPostać().equals(postać)) {
+                return postaćNaPlanszy;
+            }
+        }
+
+        throw new NieZnalezionoPostaci();
+    }
+
+    private PostaćNaPlanszy znajdźPostać(Pozycja pozycja) throws NieZnalezionoPostaci {
+        for (PostaćNaPlanszy postaćNaPlanszy : postacie) {
+            if (postaćNaPlanszy.zawiera(pozycja)) {
+                return postaćNaPlanszy;
+            }
+        }
+
+        throw new NieZnalezionoPostaci();
+    }
+
+    public void sprawdź(int wiersz, int kolumna, Akcja jeśliZajęte,
+            Runnable jeśliWolne) {
+        try {
+            PostaćNaPlanszy postać = znajdźPostać(new Pozycja(wiersz, kolumna));
+            jeśliZajęte.wykonaj(postać.dajPostać());
+        } catch (NieZnalezionoPostaci np) {
+            jeśliWolne.run();
+        }
+    }
+
+    private static class NieZnalezionoPostaci extends Exception {
+    }
 }
--- testy/ProstaPlanszaTests.java
+++ testy/ProstaPlanszaTests.java
@@ -1,6 +1,95 @@
 package testy;
 
 import gra.ProstaPlansza;
+import gra.Postać;
+import gra.Akcja;
+import sample.ProstaPostać;
+import sample.Finder;
+import sample.SpecificFinder;
 
-public class ProstaPlanszaTest {
+public class ProstaPlanszaTests {
+    public static void beginTest(String name) {
+        Testing.beginTest("ProstaPlansza." + name);
+    }
+
+    public static void checkEmpty(ProstaPlansza plansza, int row, int column,
+            int height, int width, String msg) {
+        boolean empty = true;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                Finder finder = new Finder();
+                plansza.sprawdź(row + i, column + j, finder, finder);
+                if (finder.foundPostać()) {
+                    empty = false;
+                }
+            }
+        }
+
+        Testing.checkTrue(empty, msg);
+    }
+
+    public static void checkFilled(ProstaPlansza plansza, int row, int column,
+            int height, int width, String msg) {
+        boolean filled = true;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                Finder finder = new Finder();
+                plansza.sprawdź(row + i, column + j, finder, finder);
+                if (!finder.foundPostać()) {
+                    filled = false;
+                }
+            }
+        }
+
+        Testing.checkTrue(filled, msg);
+    }
+
+    public static void checkIsSpecificPostać(ProstaPlansza plansza,
+            Postać postać, int row, int column, int height, int width,
+            String msg) {
+        boolean allSpecificPostać = true;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                SpecificFinder finder = new SpecificFinder(postać);
+                plansza.sprawdź(row + i, column + j, finder, finder);
+                if (!finder.foundPostać()) {
+                    allSpecificPostać = false;
+                }
+            }
+        }
+
+        Testing.checkTrue(allSpecificPostać, msg);
+    }
+
+
+    public static void testChęćPostawienia() {
+        beginTest("chęćPostawienia");
+
+        ProstaPlansza plansza = new ProstaPlansza(5, 5);
+
+        checkEmpty(plansza, 0, 0, 5, 5, "ProstaPlansza is empty at first");
+
+        ProstaPostać postać1 = new ProstaPostać(3, 3);
+        plansza.chęćPostawienia(postać1, 0, 0);
+        plansza.przesuńNaChcianąPozycję(postać1);
+        checkFilled(plansza, 0, 0, 3, 3, "Area of postać is filled");
+        checkIsSpecificPostać(plansza, postać1, 0, 0, 3, 3,
+               "Area of postać is all taken up by that postać");
+
+        ProstaPostać postać2 = new ProstaPostać(1, 1);
+        plansza.chęćPostawienia(postać2, 4, 3);
+        plansza.przesuńNaChcianąPozycję(postać2);
+        checkIsSpecificPostać(plansza, postać2, 4, 3, 1, 1,
+                "New postać placed correctly");
+        checkIsSpecificPostać(plansza, postać1, 0, 0, 3, 3,
+               "Old postać is still on the board");
+        checkEmpty(plansza, 3, 0, 2, 3, "Rest of the board still empty (1/3)");
+        checkEmpty(plansza, 0, 3, 4, 2, "Rest of the board still empty (2/3)");
+        checkEmpty(plansza, 4, 4, 1, 1, "Rest of the board still empty (3/3)");
+    }
+
+    public static void main(String[] args) {
+        testChęćPostawienia();
+    }
+
 }
